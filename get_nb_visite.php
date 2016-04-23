fee81672c54ef9e6b150f5a60c0d8ff876183804
Saykio <antoine.barrois1@gmail.com>
2016-04-23 11:40:14 +0200

    Update get_nb_visite.php

--- get_nb_visite.php
+++ get_nb_visite.php
@@ -41,7 +41,7 @@ if ($row_cnt > 0) {
         $product["MDP"] = $row["MDP"];*/
         // push single product into final response array
         array_push($response["visite"], $product);
-    echo "bonsoir $result";
+    
      // success
     $response["success"] = 1;
     
@@ -50,6 +50,7 @@ if ($row_cnt > 0) {
     echo json_encode($response);
      }
 } else {
+ echo "bonsoir $result";
         // no products found
     $response["success"] = 0;
     $response["message"] = "pas de conge trouvés";
